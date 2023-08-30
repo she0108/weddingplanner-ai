918813178e09fe740a1b1810c8db13e13d2a5d49
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-08-30 15:17:15 +0900

    :sparkles: WeddingPlanInitiation Section 구현
    :sparkles: WeddingPlanInitiation Section 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
 import ManualSearch from "./components/searchBlog/ManualSearch";
+import WeddingPlanInitiation from "./sections/plan/WeddingPlanInitiation";
 
 import StartButton from "./components/home/StartButton";
 
@@ -8,6 +9,7 @@ export default function Home() {
   const { data: session } = useSession();
   return (
     <div className="flex grow flex-col justify-center items-center bg-slate-50 h-screen">
+      <WeddingPlanInitiation />
       <StartButton />
     </div>
   );
